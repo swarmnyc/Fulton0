0666595f3753a94caaaf4f523a545c1e6e52fef1
ahartwel <hartwellalex@gmail.com>
2017-09-14 14:29:44 -0400

    add support for concurrency management of generated routes

--- src/app/lib/model.spec.ts
+++ src/app/lib/model.spec.ts
@@ -12,6 +12,9 @@ class TestModel extends Model {
         return {
         }
     }
+    concurrencyControl() {
+        return true
+    }
 
     getHiddenSchema() {
         return this._schema;
@@ -30,6 +33,61 @@ export class ModelTests {
         model.callUpdateHook();
         Expect(model.get("updatedAt")).toBeDefined();
         Expect(model.get("createdAt")).toBeDefined();
+    }
+
+
+    @TestCase(new TestModel({"_v": 0}))
+    public testIsAllowedToSave(model: TestModel) {
+       let canSave = model.canUpdateBasedOnConcurrencyControl({"_v": 0})
+        Expect(canSave).toEqual(true)
+    }
+
+    @TestCase(new TestModel({"_v": 1}))
+    public testIsNotAllowedToSave(model: TestModel) {
+        let canSave = model.canUpdateBasedOnConcurrencyControl({"_v": 0})
+        Expect(canSave).toEqual(false)
+    }
+
+    @TestCase({"_v": 0})
+    public testVersionIsIncrementedProperly(attr: any) {
+        let model = new TestModel()
+        let newAttr = model.updateVersion(attr)
+        Expect(newAttr["_v"]).toEqual(1)
+    }
+
+    @AsyncTest("Saving Concurrency Controlled Model")
+    async testSavingModel() {
+        addTimestamps = false
+        TestModel.prototype.save = async function() {
+            return this
+        }
+        let model = new TestModel()
+        model = await model.validate() as TestModel //will add the default _v value
+        console.log(model.schema())
+        Expect(model.get("_v")).toEqual(0)
+        model = await model.setAndValidate({"_v": 0}) as TestModel //should update _v to 1
+        Expect(model.get("_v")).toEqual(1)
+    }
+
+    @AsyncTest("Savimg Concurrency Controlled Model That Has Been Updated")
+    async testSavingModelFail() {
+        addTimestamps = false
+        TestModel.prototype.save = async function() {
+            return this
+        }
+        let model = new TestModel()
+        model = await model.validate() as TestModel //will add the default _v value
+        console.log(model.schema())
+        Expect(model.get("_v")).toEqual(0)
+
+        model.set({"_v": 1}) //fake an update
+        let e
+        try {
+            model = await model.setAndValidate({"_v": 0}) as TestModel //should update _v to 1
+        } catch(error) {
+            e = error
+        }
+        Expect(e).toBeDefined()
     }
 
    
